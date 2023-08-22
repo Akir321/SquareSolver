ace2287c01462051a774b014ae125a1fe14f7423
Akir321 <59769160+Akir321@users.noreply.github.com>
2023-08-22 14:40:21 +0300

    Add files via upload
    The first working version

---
+++ solver.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+const int INF_ROOTS = -1;
+const double EPSILON = 0.000001;
+
+int isZero (double number);
+int solveLinear(const double a, const double b,
+                    double* x1);
+int solveSquare(const  double a, const double b,
+                const double c, double* x1, double* x2);
+
+
+int isZero (double number) {
+    if (number < 0) number = -number;
+    if (number < EPSILON) return 1;
+    return 0;
+}
+
+int solveLinear(const double a, const double b, double* x1) {
+
+    assert(isfinite(a));
+    assert(isfinite(b));
+
+    assert(x1 != NULL);
+
+    if (isZero(a)) return isZero(b) ? INF_ROOTS : 0;
+    *x1 = - b / a;
+    return 1;
+}
+
+int solveSquare(const  double a, const double b,
+                const double c, double* x1, double* x2) {
+
+    assert(isfinite(a));
+    assert(isfinite(b));
+    assert(isfinite(c));
+
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    assert(x1 != x2);
+
+    if (isZero(a)) {
+        if (isZero(b)) return isZero(c) ? INF_ROOTS : 0;
+
+        return solveLinear(b, c, x1);
+    }
+
+    double D = b*b - 4*a*c;
+    if (D < 0) return 0;
+
+    if (D > 0) {
+        double sqD = sqrt(D);
+        *x1 = (-b + sqD) / 2 / a;
+        *x2 = (-b - sqD) / 2 / a;
+        return 2;
+    }
+
+    *x1 = -b / 2 / a;
+    return 1;
+}
+
+int main() {
+    printf("# Square solver\n");
+    printf("# Solves the equation ax^2 + bx + c = 0\n\n");
+    printf("Enter a, b, c: ");
+
+    double a = 0, b = 0, c = 0;
+    scanf("%lg %lg %lg", &a, &b, &c);
+
+    double x1 = 0, x2 = 0;
+    int nRoots = solveSquare(a, b, c, &x1, &x2);
+
+    switch(nRoots) {
+    case 0: printf("No roots\n");
+            break;
+
+    case 1: printf("1 root\nx = %lg\n", x1);
+            break;
+
+    case 2: printf("2 roots\nx1 = %lg\nx2 = %lg\n", x1, x2);
+            break;
+
+    case INF_ROOTS: printf("Any number\n");
+                    break;
+
+    default: printf("main(): ERROR: nRoots = %d\n", nRoots);
+             break;
+
+    }
+}
